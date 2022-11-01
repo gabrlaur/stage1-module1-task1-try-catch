30dd75220ff2cec5d0e454f732b2e5e283b91aaf
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-11-01 14:57:13 +0200

    fix exception

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -24,7 +24,7 @@ public class ParseIntegers {
             try {
                 int number = Integer.parseInt(next);
                 sum+= number;
-            } catch (Exception e) {
+            } catch (NumberFormatException e) {
                 justWords = String.join(" ", justWords, next);
             }
         }
